57d8be28a2fff10ea3974f90cdec0c1781074778
Oskar Nowak <on@ges.sh>
2018-08-02 14:44:15 +0200

    add verification IPv4

--- ipinfo/main.go
+++ ipinfo/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 type dataIP struct {
@@ -19,35 +22,61 @@ type dataIP struct {
 	Postal    string  `json:"zip"`
 }
 
-func main() {
-	var ip string
+func verificationIP(ip string) error {
+	//	if validIP6(a) == false {
+	if !validIP4(ip) {
+		err := errors.New("zle podaj prawidlowe ip")
+		return err
+	}
+	return nil
 
-	flag.StringVar(&ip, "ip", "8.8.8.9", "a string var")
-	flag.Parse()
+}
 
+func validIP4(ipAddress string) bool {
+	ipAddress = strings.Trim(ipAddress, " ")
+	re, _ := regexp.Compile(`^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`)
+	if re.MatchString(ipAddress) {
+		return true
+	}
+	return false
+}
+
+func requestIP(ip string) dataIP {
 	req, err := http.NewRequest(http.MethodGet, "http://ip-api.com/json/"+ip, nil)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 	var data dataIP
 	decoder := json.NewDecoder(resp.Body)
-
 	err = decoder.Decode(&data)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return data
+}
+
+func writingOut(data dataIP) {
 	fmt.Println("IP address: ", data.IPAddress)
-	fmt.Println("Hostname: ", data.HostName)
+	fmt.Println("Organization: ", data.HostName)
 	fmt.Println("City: ", data.City)
 	fmt.Println("Region: ", data.Region)
 	fmt.Println("Country: ", data.Coutry)
 	fmt.Println("Loc: ", data.Lon, " ", data.Lat)
 	fmt.Println("Postal: ", data.Postal)
-
+}
+func main() {
+	var ip string
+	flag.StringVar(&ip, "ip", "8.8.8.9", "a string var")
+	flag.Parse()
+	err := verificationIP(ip)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	writingOut(requestIP(ip))
 }
