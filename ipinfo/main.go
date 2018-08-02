b718985be5ce959422b14f7a855aac115821b1af
Oskar Nowak <on@ges.sh>
2018-08-02 09:54:55 +0200

    first exercise

---
+++ ipinfo/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type dataIP struct {
+	IPAddress string  `json:"query"`
+	HostName  string  `json:"org"`
+	City      string  `json:"city"`
+	Region    string  `json:"region"`
+	Coutry    string  `json:"country"`
+	Lon       float64 `json:"lon"`
+	Lat       float64 `json:"lat"`
+	Postal    string  `json:"zip"`
+}
+
+func main() {
+	var ip string
+
+	flag.StringVar(&ip, "ip", "8.8.8.8", "a string var")
+	flag.Parse()
+
+	fmt.Println("ip:", ip)
+
+	fmt.Println("http://ip-api.com/json/" + ip)
+
+	req, err := http.NewRequest(http.MethodGet, "http://ip-api.com/json/"+ip, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	var data dataIP
+	decoder := json.NewDecoder(resp.Body)
+
+	err = decoder.Decode(&data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(data)
+}
